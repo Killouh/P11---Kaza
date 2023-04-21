33d39258f54650d0aef532c9e5d84b755f7ef46b
Killouh <killlouh@gmail.com>
2023-04-21 20:06:25 +0200

    Added navlink management

--- src/views/header/Header.js
+++ src/views/header/Header.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import {Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './Header.css';
 import logo from '../../assets/LOGO.png'
 
 export default function Navbar() {
   return (
     <div className='header'>
-        <Link to="/"><img className="logoKasa" src={logo} alt="Logo de Kasa" /></Link>
-        <nav>
-        <Link className="navText" to="/"> Accueil </Link>
-        <Link className="navText" to="/Propos"> A propos</Link>
-    </nav>
+      <Link to="/">
+        <img className="logoKasa" src={logo} alt="Logo de Kasa" />
+      </Link>
+      <nav>
+        <NavLink to="/" className="navText" activeclassname="activeLink">
+          Accueil
+        </NavLink>
+        <NavLink to="/Propos" className="navText" activeclassname="activeLink">
+          A propos
+        </NavLink>
+      </nav>
     </div>
   )
 }
